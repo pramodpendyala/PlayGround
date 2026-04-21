e5241fe3186fc7a0ab34dcd0d3892ee9645d7436
Pramod Pendyala <devpramod3@gmail.com>
2026-04-21 17:34:49 -0400

    just now

--- playground/force-app/main/default/lwc/dynamicDetailForm/dynamicDetailForm.js
+++ playground/force-app/main/default/lwc/dynamicDetailForm/dynamicDetailForm.js
@@ -1,39 +1,22 @@
-import { LightningElement, api, wire, track } from 'lwc';
-import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import getConfiguration from '@salesforce/apex/DynamicDetailFormController.getConfiguration';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { LightningElement, api, wire } from 'lwc';
+import getFormConfiguration from '@salesforce/apex/DynamicDetailFormController.getFormConfiguration';
 
 export default class DynamicDetailForm extends LightningElement {
     @api recordId;
-    @api objectApiName; // Passed from App Builder or Parent
-    @track fields;
+    @api objectApiName; // Passed automatically if on record page
 
-    @wire(getObjectInfo, { objectApiName: '$objectApiName' })
-    objectInfo;
+    formLayout;
+    error;
 
-    get objectLabel() {
-        return this.objectInfo.data ? this.objectInfo.data.label : '';
-    }
-    get title(){
-        return this.objectLabel+' Details.'
-    }
-    @wire(getConfiguration, { objectApiName: '$objectApiName' })
-    wiredFields({ error, data }) {
+    @wire(getFormConfiguration, { objectApiName: '$objectApiName' })
+    wiredConfig({ error, data }) {
         if (data) {
-            console.log('Fields received==', JSON.stringify(data));
-            this.fields = data;
+            this.formLayout = data;
+            this.error = undefined;
         } else if (error) {
-            console.error('Error fetching metadata==', error);
+            this.error = error;
+            this.formLayout = undefined;
+            console.error('Error loading dynamic form:', error);
         }
-    }
-
-    handleSuccess(event) {
-        this.dispatchEvent(
-            new ShowToastEvent({
-                title: 'Success',
-                message: 'Record updated successfully!',
-                variant: 'success',
-            })
-        );
     }
 }
